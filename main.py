1df25211f0a08d21a89c777c006ff805bfd3ac86
mssoheil <mssoheil88@yahoo.com>
2024-06-06 11:49:50 +0330

    feat: implement slot machine

--- main.py
+++ main.py
@@ -1,13 +1,66 @@
+
+import random
+from validate_util import validateNumeric
+
 MIN_BET = 1
 MAX_BET = 100
-
-from validate_util import validateNumeric
 
 MAX_LINES = 3
 MIN_LINES = 1
 
 MIN_DEPOSIT_AMOUNT = 1
 MAX_DEPOSIT_AMOUNT = 1000000
+
+ROWS = 3
+COLS = 3
+
+SYMBOL_COUNT = {
+	"A": 2,
+	"B": 4,
+	"C": 6,
+	"D": 8,
+}
+
+def getAllSymbols(symbols: dict[str, int]):
+	allSymbols: list[str] = []
+
+	for symbol, symbolCount in symbols.items():
+		for _ in range(symbolCount):
+			allSymbols.append(symbol)
+
+	return allSymbols
+
+def getColumns(cols: int, rows: int, allSymbols: list[str]):
+	"""Creating symbols for each column of each rows of slot machine"""
+	columns: list[list[str]] = []
+
+	for _ in range(cols):
+		column: list[str] = []
+		currentSymbols = allSymbols[:]
+		for _ in range(rows):
+			value = random.choice(currentSymbols)
+			currentSymbols.remove(value)
+			column.append(value)
+
+		columns.append(column)
+
+	return columns
+
+def getSlotMachineSpin(cols: int, rows: int, symbols: dict[str, int]):
+	allSymbols = getAllSymbols(symbols)
+
+	columns = getColumns(cols, rows, allSymbols)
+
+	return columns
+
+def printSlotMachine(columns: list[list[str]] ):
+	# transposing the columns to reverse it to become rows
+	for row in range(len(columns[0])):
+		for i, column in enumerate(columns):
+
+			print(column[row], end = " | " if i != len(column) - 1 else "")
+
+		print()
 
 def validateDepositAmount(amount: str):
 	isValidNumber = validateNumeric(amount, f"Amount should be between {MIN_DEPOSIT_AMOUNT}-{MAX_DEPOSIT_AMOUNT}", MIN_DEPOSIT_AMOUNT, MAX_DEPOSIT_AMOUNT)
@@ -91,6 +144,9 @@ def main():
 	totalBet = bet * lines
 
 	print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${totalBet}")
+
+	slots = getSlotMachineSpin(COLS, ROWS, SYMBOL_COUNT)
+	printSlotMachine(slots)
 
 if __name__ == "__main__":
 	main()
